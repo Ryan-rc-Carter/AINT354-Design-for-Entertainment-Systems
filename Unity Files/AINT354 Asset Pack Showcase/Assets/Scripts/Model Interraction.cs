fa83616d60ad3b7084917c8daf1dfe2405922103
Ryan-rc-Carter <33025230+Ryan-rc-Carter@users.noreply.github.com>
2019-12-08 17:38:07 +0000

    Model view scene

---
+++ Unity Files/AINT354 Asset Pack Showcase/Assets/Scripts/Model Interraction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelInterraction : MonoBehaviour    
+{
+
+    private Transform modelPlatform;
+
+    [SerializeField]
+    private float rotationMultiplier = 1.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        modelPlatform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            modelPlatform.Rotate(Vector3.right * Time.deltaTime * rotationMultiplier);
+        }
+    }
+}
---
+++ Unity Files/AINT354 Asset Pack Showcase/Assets/Scripts/PlatformInterraction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformInterraction : MonoBehaviour
+{
+    
+    [SerializeField]
+    private float rotationMultiplier = 50f;
+
+    [SerializeField]
+    private float smoothing = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, rotationMultiplier * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, -rotationMultiplier * Time.deltaTime);
+        }
+
+    }
+}
